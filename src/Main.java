c65863033d724d6ebfe172db3262d94958fee012
Jaymin <jayminwest@gmail.com>
2021-01-28 09:40:13 -0800

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/*
+        Using Git with IntelliJ
+            Go to VCS, click enable "version control integration"
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello again");
+    }
+}
